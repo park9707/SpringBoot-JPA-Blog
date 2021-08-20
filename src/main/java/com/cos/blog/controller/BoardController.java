894a21234c5891ff5024fc6b4007980b97d76fb9
park9707 <psw1436@naver.com>
2021-08-20 16:41:32 +0900

    시큐리티 설정 마무리

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,5 +1,7 @@
 package com.cos.blog.controller;
 
+import com.cos.blog.config.auth.PrincipalDetail;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BoardController {
 
     @GetMapping({"","/"})
-    public String index(){
+    public String index(@AuthenticationPrincipal PrincipalDetail principal){ //컨트롤러에서 세션을 어떻게 찾는지?
         return "index";
     }
 }
