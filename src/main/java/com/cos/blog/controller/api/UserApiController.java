0da00b23a274bcc7452c24546309020f3b680ce4
park9707 <psw1436@naver.com>
2021-08-23 21:45:32 +0900

    회원정보수정 마무리 (세션등록)

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,24 +1,27 @@
 package com.cos.blog.controller.api;
 
 import com.cos.blog.dto.ResponseDto;
-import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpSession;
 
 @RestController
 public class UserApiController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @PostMapping("/auth/joinProc")
     public ResponseDto<Integer> save(@RequestBody User user){
@@ -29,6 +32,13 @@ public class UserApiController {
     @PutMapping("/user")
     public ResponseDto<Integer> update(@RequestBody User user){
         userService.회원수정(user);
+        // 여기서는 트랜잭션이 종료되기 때문에 DB에 값은 변경이 됐음
+        // 세션값은 변경되지 않은 상태이기 때문에 로그아웃하고 다시 로그인해야 변경된걸로 보이므로 직접 세션값을 변경해줘야한다.
+        // 세션등록
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
         return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
     }
+
 }
